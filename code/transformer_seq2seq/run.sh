146af3dd73f061f731ff6362af540d58ffff808a
Kaden Griffith <kadengriffith@gmail.com>
2022-06-23 21:05:39 -0600

    Remove committed cache files Fixup transformer

--- code/transformer_seq2seq/run.sh
+++ code/transformer_seq2seq/run.sh
@@ -17,11 +17,13 @@ else
         -mode train \
         -embedding roberta \
         -emb_name roberta-base \
-        -emb1_size 768 \
         -emb_lr 8e-6 \
-        -hidden_size 256 \
-        -depth 2 \
-        -lr 0.0002 \
+        -heads 8 \
+        -d_model 512 \
+        -d_ff 2048 \
+        -encoder_layers 6 \
+        -decoder_layers 6 \
+        -lr 1e-8 \
         -batch_size 4 \
         -epochs ${1} \
         -early_stopping ${1} \
