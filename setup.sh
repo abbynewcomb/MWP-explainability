0f1be37a353cc7b24ffa512d8edc976bd1c7dfb7
Abby Newcomb <abby@MAIN402b-lap1.uccs.edu>
2022-06-24 16:15:47 -0600

    removing unnecessary model and working setup script

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
-pip install virtualenv
+sudo pip3 install virtualenv
 
 virtualenv -p python3 venv && source venv/bin/activate
 
-pip install -r code/requirements.txt
+pip3 install -r code/requirements.txt
 
-for FILE in `find code -name setup.sh -type f`; do
-    echo "Running ${FILE}..."
-    sh $(basename ${FILE})
-done
+(cd code/graph2tree; mkdir {logs,models,out,runs,outputs})
+(cd code/gts; mkdir {logs,models,out,runs,outputs})
+(cd code/rnn_seq2seq; mkdir {logs,models,out,runs,outputs})
+(cd code/transformer_seq2seq; mkdir {logs,models,out,runs,outputs})
