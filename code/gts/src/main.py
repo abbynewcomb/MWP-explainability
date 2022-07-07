74bf6afd4f8012442e0e5e92f30ab7fa2d01e19d
Kaden Griffith <kadengriffith@gmail.com>
2022-07-06 19:17:33 -0600

    chore: Fixes gts testing

--- code/gts/src/main.py
+++ code/gts/src/main.py
@@ -653,26 +653,16 @@ def main():
                     )
                 )
 
-            if config.results:
-                store_results(
-                    config,
-                    max_train_acc,
-                    max_val_acc,
-                    eq_acc,
-                    min_train_loss,
-                    best_epoch,
-                )
-                logger.info("Scores saved at {}".format(config.result_path))
-
             best_acc.append((max_value_corr, len_total_eval))
 
         total_value_corr = 0
         total_len = 0
+
         for w in range(len(best_acc)):
-            folds_scores.append(float(best_acc[w][0]) / best_acc[w][1])
+            folds_scores.append(float(best_acc[w][0]) / (best_acc[w][1] or 1))
             total_value_corr += best_acc[w][0]
             total_len += best_acc[w][1]
-        fold_acc_score = float(total_value_corr) / total_len
+        fold_acc_score = float(total_value_corr) / (total_len or 1)
 
         store_val_results(config, fold_acc_score, folds_scores)
         logger.info("Final Val score: {}".format(fold_acc_score))
